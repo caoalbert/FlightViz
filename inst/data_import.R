c93d4264bd1bd2ab15f91fc7113966eefa62573c
caoalbert <zyfxdtn@gmail.com>
2022-01-10 02:24:23 -0800

    manufacture selector added

--- inst/data_import.R
+++ inst/data_import.R
@@ -1,18 +1,18 @@
 df1<- data.frame(
-  Dep_Airport = c("LAX", "LAX", "DFW"),
-  Arvl_Airport = c("HNL", "PEK", "JFK"),
-  Airline_cd = rep("AA", 3),
-  Airline = rep("American Airlines", 3),
-  Manufacture = rep("Airbus", 3),
-  Model = rep("A320", 3),
-  Subtype = rep("A321", 3),
-  Alti = c(0.1,0.4,0.8)
+  Dep_Airport = c("LAX", "LAX", "DFW", "CVG"),
+  Arvl_Airport = c("HNL", "PEK", "JFK", "SFO"),
+  Airline_cd = rep("AA", 4),
+  Airline = rep("American Airlines", 4),
+  Manufacture = c(rep("Airbus", 3), "Boeing"),
+  Model = c(rep("A320", 3), "B737"),
+  Subtype = c(rep("A321", 3), "B737-8"),
+  Alti = c(0.1,0.4,0.8,0.2)
 )
 
 df2<- df1 %>%
   left_join(airports, by = c("Dep_Airport" = "IATA")) %>%
   left_join(airports, by = c("Arvl_Airport" = "IATA"), suffix = c("_dep", "_arvl")) %>%
-  select(Lat_dep, Long_dep, Lat_arvl, Long_arvl, Alti)
+  select(Lat_dep, Long_dep, Lat_arvl, Long_arvl, Alti, Manufacture)
 
 
 
--- inst/shiny_example.R
+++ inst/shiny_example.R
@@ -8,20 +8,25 @@ create_globe(df2) %>%
 
 
 ui<- fluidPage(
-  actionButton("add", "add arcs"),
+  selectInput("manufacture", "Manufacture", choices = levels(factor(df2$Manufacture))),
   globeOutput("globe")
 )
+
+
 server<- function(input, output){
+  df<- reactive({
+    df2 %>% filter(Manufacture == input$manufacture)
+  })
+
+
   output$globe<- renderGlobe({
-    create_globe(df2) %>%
+    create_globe(df()) %>%
       globe_arcs(coords(start_lat = Lat_dep,
                         start_lon = Long_dep,
                         end_lat = Lat_arvl,
                         end_lon = Long_arvl)) %>%
-      arcs_altitude_scale(0.2)}
-  )
-
-
+      arcs_altitude_scale(0.2)
+  })
 }
 
 shinyApp(ui, server)
